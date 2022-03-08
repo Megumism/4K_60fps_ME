65bc6256a7e4f64ecfb3519bee0b46d6575803af
ToCatRow <y15565631047@sjtu.edu.cn>
2022-03-08 08:18:51 +0800

    更新了sad模块

--- core/pe.v
+++ core/pe.v
@@ -1,9 +1,44 @@
-module pe (
-    input wire [8-1:0] crt_pixel_i,
-    input wire [8-1:0] pre_pixel_i,
-    output wire [8-1:0] crt_pixel_o,
-    output wire [8-1:0] pre_pixel_o,
-    output wire [8-1:0] ad 
-);
+module pe (input wire clk,
+           input wire rst,
+           input wire crt_keep,
+           input wire [8-1:0] crt_pixel_i,
+           input wire [8-1:0] pre_pixel_i,
+           output wire [8-1:0] crt_pixel_o,
+           output wire [8-1:0] pre_pixel_o,
+           output wire [8-1:0] ad);
     
+    //定义内部用于计算的当前帧像素信号
+    reg [3-1:0] crt_pixel_cal;
+    always @(posedge clk) begin
+
+        if(rst) crt_pixel_cal <= 0;
+
+        else begin
+            if(crt_keep == 0) crt_pixel_cal <= crt_pixel_i;
+        end
+    
+    end
+
+    //定义内部用于计算的先前帧像素信号
+    reg [3-1:0] pre_pixel_cal;
+    always @(posedge clk) begin
+
+        if(rst) pre_pixel_cal <= 0;
+
+        else pre_pixel_cal <= pre_pixel_i;
+
+    end
+
+    //定义计算结果AD值
+    assign ad = (crt_keep == 1)?((crt_pixel_cal > pre_pixel_cal)
+                                ?(crt_pixel_cal - pre_pixel_cal)
+                                :(pre_pixel_cal - crt_pixel_cal))
+                               :0;
+
+    //定义输出的当前帧像素信号
+    assign crt_pixel_o = crt_pixel_cal;
+
+    //定义输出的先前帧像素信号
+    assign pre_pixel_o = pre_pixel_cal;
+
 endmodule
